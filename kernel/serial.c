e8879f17acf43727decd676ca3a450e6745028e7
aldrsun <147417224+aldrsun@users.noreply.github.com>
2025-09-08 02:34:17 +0300

    Update serial.c

--- kernel/serial.c
+++ kernel/serial.c
@@ -1,10 +1,5 @@
 #include <io.h>
 
-/* The I/O ports */
-/* All the I/O ports are calculated relative to the data port. This is because
-* all serial ports (COM1, COM2, COM3, COM4) have their ports in the same
-* order, but they start at different values.
-*/
 #define SERIAL_COM1_BASE 0x3F8 /* COM1 base port */
 
 #define SERIAL_DATA_PORT(base) (base)
@@ -13,7 +8,6 @@
 #define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
 #define SERIAL_LINE_STATUS_PORT(base) (base + 5)
 
-/* The I/O port commands */
 /* SERIAL_LINE_ENABLE_DLAB:
 * Tells the serial port to expect first the highest 8 bits on the data port,
 * then the lowest 8 bits will follow
@@ -24,9 +18,6 @@
  * Sets the speed of the data being sent. The default speed of a serial
  * port is 115200 bits/s. The argument is a divisor of that number, hence
  * the resulting speed becomes (115200 / divisor) bits/s.
- *
- * @param com The COM port to configure
- * @param divisor The divisor
  */
 void serial_configure_baud_rate(unsigned short com, unsigned short divisor) {
     outb(SERIAL_LINE_COMMAND_PORT(com),
@@ -41,15 +32,9 @@ void serial_configure_baud_rate(unsigned short com, unsigned short divisor) {
 * Configures the line of the given serial port. The port is set to have a
 * data length of 8 bits, no parity bits, one stop bit and break control
 * disabled.
-*
-* @param com The serial port to configure
 */
 void serial_configure_line(unsigned short com)
 {
-  /* Bit:     | 7 | 6 | 5 4 3 | 2 | 1 0 |
-   * Content: | d | b | prty  | s | dl  |
-   * Value:   | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
-   */
   outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
 }
 
@@ -71,10 +56,6 @@ void serial_init() {
 /** serial_is_transmit_fifo_empty:
 * Checks whether the transmit FIFO queue is empty or not for the given COM
 * port.
-*
-* @param com The COM port
-* @return 0 if the transmit FIFO queue is not empty
-* 1 if the transmit FIFO queue is empty
 */
 int serial_is_transmit_fifo_empty(unsigned int com)
 {
